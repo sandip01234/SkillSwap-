449bfe0916623fca881eb66dee8e0b5d36fb82e9
sandip01234 <141340282+sandip01234@users.noreply.github.com>
2025-08-07 13:46:16 +0545

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'sandip@123');
+define('DB_PASS', '');
 define('DB_NAME', 'skillswap');
 define('BASE_URL', 'http://localhost/SkillSwap/');
